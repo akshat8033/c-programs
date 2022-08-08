744db90b533b16256b31615bbaac713eac1ee1ab
akshat8033 <110470545+akshat8033@users.noreply.github.com>
2022-08-08 18:59:44 +0530

    Add files via upload

---
+++ fibbonacci using method.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Demo
+{
+    void Fibo(int num)
+    {
+        int n1 = 0, n2 = 1, n3;
+
+        Console.WriteLine(n1);
+        Console.WriteLine(n2);
+        for (int i = 2; i < num; i++)
+        {
+            n3 = n2 + n1;
+            Console.WriteLine(n3);
+            n1 = n2;
+            n2 = n3;
+}
+           
+
+        
+
+
+
+    }
+    static void Main()
+    {
+
+        Console.WriteLine("enter the total digits: ");
+        int a = int.Parse(Console.ReadLine());
+        
+        Demo d = new Demo();
+        d.Fibo(a);
+        
+    }
+}
